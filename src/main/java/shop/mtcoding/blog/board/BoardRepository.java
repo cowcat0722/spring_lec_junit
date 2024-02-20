994fb15b120e10c91a12ec5847e4f015ccfebf8a
Pakchan <1625787@donga.ac.kr>
2024-02-20 15:41:58 +0900

    select Test

--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -6,13 +6,38 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Repository
 public class BoardRepository {
     private final EntityManager em;
 
+    public List<Board> selectAll() {
+        Query query = em.createNativeQuery("select * from board_tb ", Board.class);
+
+        try {
+            List<Board> board = (List<Board>) query.getResultList();
+            return board;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public Board selectOne(int id) {
+        Query query = em.createNativeQuery("select * from board_tb where id = ?", Board.class);
+        query.setParameter(1, id);
+
+        try {
+            Board board = (Board) query.getSingleResult();
+            return board;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
     @Transactional
-    public void insert(String title, String content, String author){
+    public void insert(String title, String content, String author) {
         Query query = em.createNativeQuery("insert into board_tb(title, content, author) values(?, ?, ?)");
         query.setParameter(1, title);
         query.setParameter(2, content);
